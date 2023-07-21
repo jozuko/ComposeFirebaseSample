ffc7adf93db4ab4291f3561344fea6184247c4e9
jozuko <jozuko.dev@gmail.com>
2023-07-21 15:16:53 +0900

    apply hilt

---
+++ app/src/main/java/com/jozu/compose/firebasesample/FirebaseApplication.kt
@@ -0,0 +1,12 @@
+package com.jozu.compose.firebasesample
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ *
+ * Created by jozuko on 2023/07/21.
+ * Copyright (c) 2023 Studio Jozu. All rights reserved.
+ */
+@HiltAndroidApp
+class FirebaseApplication : Application()
